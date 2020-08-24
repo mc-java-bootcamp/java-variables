6e0ebd819da7bc6388bb27da7f26e12860b78045
yerx <yer.x.xiong@gmail.com>
2020-08-24 17:01:13 -0400

    completed exercise

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int testScore = 89;
+        double price = 34.22;
+        String userName = "Joe";
+
+        System.out.println("Test Score initialized is: " + testScore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + userName);
+
+    }
+
+}
